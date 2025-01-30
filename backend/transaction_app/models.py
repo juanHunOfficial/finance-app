e13c1e4f0bc14c03ca5431518185f2ffeac7e29d
JUAN_HUN <juanhun2424@gmail.com>
2025-01-30 13:53:37 -0500

    Connect the transactions model to the User model

--- backend/transaction_app/models.py
+++ backend/transaction_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from user_app.models import User
 
 # Create your models here.
 class Transaction(models.Model):
   description = models.CharField(max_length=255)
   amount = models.DecimalField(max_digits=10, decimal_places=2)
-  date = models.DateField()
+  date = models.DateField() #template "2025-01-30"
+  user = models.ForeignKey(User, related_name='transactions', on_delete=models.CASCADE)
