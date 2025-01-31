75923daaf4fdc20b9228237fd7873d01ad2f393b
JUAN_HUN <juanhun2424@gmail.com>
2025-01-31 12:57:16 -0500

    delete django backend, add node backend

--- backend/monthly_sum_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MonthlySumAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'monthly_sum_app'
--- backend/transaction_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from category_app.models import Category
-
-# Create your models here.
-class Transaction(models.Model):
-  description = models.CharField(max_length=255)
-  amount = models.DecimalField(max_digits=10, decimal_places=2)
-  date = models.DateField() #template "2025-01-30"
-  category = models.ForeignKey(Category, related_name='transactions', on_delete=models.CASCADE)
